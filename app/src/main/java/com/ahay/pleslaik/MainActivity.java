e68445c146087dc051d26e1eb3962ee405c42843
Sugihars0 <sugiharsowijaya@gmail.com>
2019-06-25 18:32:47 +0700

    Initial commit

---
+++ app/src/main/java/com/ahay/pleslaik/MainActivity.java
@@ -0,0 +1,58 @@
+package com.ahay.pleslaik;
+
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tulisan;
+    private SwitchCompat OnOffSwitch;
+    private CameraManager mCameraManager;
+    private String mCameraId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getting the camera manager and camera id
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        OnOffSwitch = findViewById(R.id.OnOffSwitch);
+        tulisan = findViewById(R.id.tulisan);
+
+        OnOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    tulisan.setVisibility(View.VISIBLE);
+                }else{
+                    tulisan.setVisibility(View.INVISIBLE);
+                }
+                switchFlashLight(isChecked);
+            }
+        });
+
+    }
+
+    public void switchFlashLight(boolean status) {
+        try {
+            mCameraManager.setTorchMode(mCameraId, status);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
